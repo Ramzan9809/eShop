4c0ceb302e948c2a2536672d581676062281f9bd
Ramzan9809 <ramzan.rgwctx@gmail.com>
2025-01-18 21:53:00 +0600

    done product model, setting admin.py

--- apps/carts/admin.py
+++ apps/carts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
--- apps/carts/models.py
+++ apps/carts/models.py
@@ -12,7 +12,7 @@ class Category(MPTTModel):
     title = models.CharField(max_length=50, verbose_name='Название категории')
     keywords = models.CharField(max_length=255, verbose_name='Ключевые слова')
     image = models.ImageField(blank=True, upload_to='images/')
-    status = models.CharField(max_length=10, choices=STATUS)
+    status = models.CharField(max_length=10, choices=STATUS, default=True)
     slug = models.SlugField(null=False, unique=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
@@ -21,4 +21,32 @@ class Category(MPTTModel):
     
     def get_absolute_url(self):
         return reverse('category_detail', kwargs={'slug': self.slug})
+    
+    class Meta:
+        verbose_name_plural = 'Категории'
+        verbose_name = 'категория'
+    
+class Product(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, verbose_name='Название продукта')
+    keywords = models.CharField(max_length=255, verbose_name='Ключевые слова')
+    image = models.ImageField(blank=False, upload_to='images/')
+    descriptions = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.ImageField(null=False, unique=True)
+    create_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'slug': self.slug})
+
+    class Meta:
+        verbose_name_plural = 'Продукты'
+        verbose_name = 'продукт'
